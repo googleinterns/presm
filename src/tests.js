91e23c471a45ecb3540ce2022306010a7045cb1c
Julian Londono <jlondono@google.com>
2020-06-08 13:38:56 -0700

    Refactor unit tests to test command line output

--- src/tests.js
+++ src/tests.js
@@ -3,36 +3,84 @@ const require = createRequire(import.meta.url);
 
 let test = require('tape');
 
-import { ops_list } from "./ops.js"
+import { calc } from "./calc.js"
+
+let {argv} = require('yargs').config({});
+
+function modifyTestArgv(op, a, b){
+    delete argv.add;
+    delete argv.sub;
+    delete argv.div;
+    delete argv.mult;
+    argv[op] = [a, b];
+    return argv
+}
+
+
+function createCorrectOutput(op, op_symbol, a, b, res){
+    return `${op} ${a}${op_symbol}${b} = ${res}`
+}
 
 test('add test', function (t) {
     t.plan(2);
 
-    t.equal(ops_list["add"](5,10), 15);
+    let op = "add";
+    let op_symbol = "+"
+    let a = 5;
+    let b = 10;
+    let res = 15;
+    t.equal(calc(modifyTestArgv(op, a, b)), createCorrectOutput(op, op_symbol, a, b, res));
 
-    t.equal(ops_list["add"](150,0), 150);
+    a = 15;
+    b = 0;
+    res = 15;
+    t.equal(calc(modifyTestArgv(op, a, b)), createCorrectOutput(op, op_symbol, a, b, res));
 });
 
 test('sub test', function (t) {
     t.plan(2);
 
-    t.equal(ops_list["sub"](5,10), -5);
+    let op = "sub";
+    let op_symbol = "-"
+    let a = 5;
+    let b = 10;
+    let res = -5;
+    t.equal(calc(modifyTestArgv(op, a, b)), createCorrectOutput(op, op_symbol,a, b, res));
 
-    t.equal(ops_list["sub"](150,0), 150);
+    a = 15;
+    b = 0;
+    res = 15;
+    t.equal(calc(modifyTestArgv(op, a, b)), createCorrectOutput(op, op_symbol,a, b, res));
 });
 
 test('mult test', function (t) {
     t.plan(2);
 
-    t.equal(ops_list["mult"](5,10), 50);
+    let op = "mult";
+    let op_symbol = "*"
+    let a = 5;
+    let b = 10;
+    let res = 50;
+    t.equal(calc(modifyTestArgv(op, a, b)), createCorrectOutput(op, op_symbol,a, b, res));
 
-    t.equal(ops_list["mult"](150,0), 0);
+    a = 15;
+    b = 0;
+    res = 0;
+    t.equal(calc(modifyTestArgv(op, a, b)), createCorrectOutput(op, op_symbol,a, b, res));
 });
 
 test('div test', function (t) {
     t.plan(2);
 
-    t.equal(ops_list["div"](5,10), 0.5);
+    let op = "div";
+    let op_symbol = "/"
+    let a = 5;
+    let b = 10;
+    let res = 0.5;
+    t.equal(calc(modifyTestArgv(op, a, b)), createCorrectOutput(op, op_symbol,a, b, res));
 
-    t.equal(ops_list["div"](0,150), 0);
+    a = 15;
+    b = 0;
+    res = "Infinity";
+    t.equal(calc(modifyTestArgv(op, a, b)), createCorrectOutput(op, op_symbol,a, b, res));
 });
