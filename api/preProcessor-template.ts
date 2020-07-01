9c588c32759cfacd6b631c02bc5ac266af6f8541
Julian Londono <jlondono@google.com>
2020-07-01 18:40:16 +0000

    Update files using prettier

--- api/preProcessor-template.ts
+++ api/preProcessor-template.ts
@@ -1,37 +1,37 @@
 import thing from 'module';
 
-export let sourceExtensionTypes: string[] = [".ts", ".tsx"];
+export let sourceExtensionTypes: string[] = ['.ts', '.tsx'];
 
-export let outputExtensionTypes: string[] = [".mjs", ".cjs"];
-
+export let outputExtensionTypes: string[] = ['.mjs', '.cjs'];
 
 // Example of defining "options" for a TypeScript loader
 export interface TypeScriptOptions {
-  tsconfigFile?: string
+  tsconfigFile?: string;
 }
 
 interface ProcessedFile {
-  source: string,
-  extension?: string
-};
+  source: string;
+  extension?: string;
+}
 
 interface preProcessorInstance {
-  process: Function
-};
+  process: Function;
+}
 
-export function getPreprocessor(options: TypeScriptOptions = {}): preProcessorInstance {
+export function getPreprocessor(
+  options: TypeScriptOptions = {}
+): preProcessorInstance {
   // Create instances and state variables
-  // E.g. a CompilerHost instance to compile TypeScript 
-
+  // E.g. a CompilerHost instance to compile TypeScript
 
   // Example of returning a transpiled source using options provided by project config
-  return { 
-          process(source: string): ProcessedFile{
-            return {
-                    source: thing.transpileSource(source, options),
-                    extension: outputExtensionTypes[0]  // Example of choosing 
-                                                        // a valid extension from possible extensions}
-                  }
-        }
-  }
+  return {
+    process(source: string): ProcessedFile {
+      return {
+        source: thing.transpileSource(source, options),
+        extension: outputExtensionTypes[0], // Example of choosing
+        // a valid extension from possible extensions}
+      };
+    },
+  };
 }
