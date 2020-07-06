81cd7072d47846639f3bb4a5d8974bc2fecb9ee3
Julian Londono <jlondono@google.com>
2020-07-06 20:48:39 +0000

    Prettify template

--- api/postProcessor-template.ts
+++ api/postProcessor-template.ts
@@ -1,36 +1,36 @@
 import thing from 'module';
 
-export let sourceExtensionTypes: string[] = [".ts", ".tsx"];
+export let sourceExtensionTypes: string[] = ['.ts', '.tsx'];
 
-export let outputExtensionTypes: string[] = [".mjs", ".cjs"];
-
+export let outputExtensionTypes: string[] = ['.mjs', '.cjs'];
 
 // Example of defining "options" for a TypeScript loader
 export interface BabelOptions {
-  tsconfigFile?: string
+  tsconfigFile?: string;
 }
 
 interface ProcessedFile {
-  source: string,
-  extension?: string
-};
+  source: string;
+  extension?: string;
+}
 
 interface postProcessorInstance {
-  process: Function
-};
+  process: Function;
+}
 
-export function getPreprocessor(options: BabelOptions = {}): postProcessorInstance {
-  // Create instances and state variables 
-
+export function getPreprocessor(
+  options: BabelOptions = {}
+): postProcessorInstance {
+  // Create instances and state variables
 
   // Example of returning a transpiled source using options provided by project config
-  return { 
-          process(source: string): ProcessedFile{
-            return {
-                    source: thing.transpileSource(source, options),
-                    extension: outputExtensionTypes[0]  // Example of choosing 
-                                                        // a valid extension from possible extensions}
-                  }
-        }
-  }
+  return {
+    process(source: string): ProcessedFile {
+      return {
+        source: thing.transpileSource(source, options),
+        extension: outputExtensionTypes[0], // Example of choosing
+        // a valid extension from possible extensions}
+      };
+    },
+  };
 }
