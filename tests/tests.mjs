e7982d6b2cd48a4356d0a2fb344f5a262f8d9807
Julian Londono <jlondono@google.com>
2020-07-14 22:32:55 +0000

    Add unit tests for YAML loader

--- tests/tests.mjs
+++ tests/tests.mjs
@@ -4,22 +4,54 @@ const loadViaRequire = createRequire(import.meta.url);
 // import test from 'tape';
 const test = loadViaRequire('tape');
 
-const {argv} = loadViaRequire('yargs').config({});
+import {moduleWrapper} from '../src/utils.mjs';
+import {promises as fs} from 'fs';
+import url from 'url';
 
-// Imports for tests
-import testYAMLFile1 from './testfiles/yamlExample.yaml';
-import testYAMLFile2 from './testfiles/yamlExample.yml';
+// Tests exports that all Pre-Processors should have
+// Returns insatnce of getPreProcessor if successful
+function testPreProcessorExports(t, preprocessor, options) {
+  t.ok(preprocessor.sourceExtensionTypes, 'Exports sourceExtensionTypes');
+  t.ok(preprocessor.outputExtensionTypes, 'Exports outputExtensionTypes');
+  t.ok(preprocessor.getPreProcessor, 'Exports getPreProcessor()');
 
-test('YAML Loader: Import YAML File', async t => {
-  t.plan(2);
+  let preprocessorInstance = preprocessor.getPreProcessor(options);
+  t.ok(preprocessorInstance.process, 'Exports process()');
 
-  let truthYAMLFile = {
-    Employees: [
-      {'John Doe': {job: 'SWE', skills: ['python', 'java']}},
-      {'Jane Doe': {job: 'SWE', skills: ['java', 'python', 'php']}},
-    ],
-  };
+  return preprocessorInstance;
+}
 
-  t.deepEquals(truthYAMLFile, testYAMLFile1);
-  t.deepEquals(truthYAMLFile, testYAMLFile2);
+test('[Unit] YAML Loader: ', async t => {
+  let preProcessorYAML = await import(
+    '../examples/loaders/preprocessor-yaml.mjs'
+  );
+
+  let preProcessorYAMLInstance = testPreProcessorExports(
+    t,
+    preProcessorYAML,
+    {}
+  );
+
+  let urlYAML = url.pathToFileURL('./tests/testfiles/yamlExample.yaml');
+  let rawYAMLSource = await fs.readFile(new URL(urlYAML), 'utf8');
+
+  let jsonYAMLOutput = (await preProcessorYAMLInstance.process(rawYAMLSource))
+    ?.source;
+
+  let truthYAMLJSON = moduleWrapper(
+    JSON.stringify({
+      Employees: [
+        {'John Doe': {job: 'SWE', skills: ['python', 'java']}},
+        {'Jane Doe': {job: 'SWE', skills: ['java', 'python', 'php']}},
+      ],
+    })
+  );
+
+  t.deepEquals(
+    truthYAMLJSON,
+    jsonYAMLOutput,
+    'Converts YAML to JSON correctly'
+  );
+
+  t.end();
 });
