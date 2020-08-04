49036c87c21a21cb0f30012e5769f8e13a8723c8
Julian Londono <jlondono@google.com>
2020-08-04 01:23:21 +0000

    Create 'dist' dir if nonexistent

--- test/fixtures/buildModePlaceholder.mjs
+++ test/fixtures/buildModePlaceholder.mjs
@@ -1,15 +1,21 @@
-import {promises as fs} from 'fs';
+import {promises} from 'fs';
 import {pathToFileURL} from 'url';
+import fs from 'fs';
 
 async function main() {
   const buildURL = pathToFileURL('./dist/test.mjs');
+  const buildDir = './dist';
 
   const buildCode =
     "import ts from 'typescript';\n\
     const tsTarget = `${ts.ScriptTarget.ES2020}`;\n\
     console.log('TS target', tsTarget);";
 
-  await fs.writeFile(buildURL, buildCode, 'utf8');
+  if (!fs.existsSync(buildDir)) {
+    fs.mkdirSync(buildDir);
+  }
+
+  await promises.writeFile(buildURL, buildCode, 'utf8');
 }
 
 main();
