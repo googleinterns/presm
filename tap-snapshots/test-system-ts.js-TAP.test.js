4e8ffcc5d789e35f9e827453149e3d2478f092b9
Julian Londono <jlondono@google.com>
2020-08-14 21:36:23 +0000

    Add debug flag to resourceprovider output

--- tap-snapshots/test-system-ts.js-TAP.test.js
+++ tap-snapshots/test-system-ts.js-TAP.test.js
@@ -6,10 +6,6 @@
  */
 'use strict'
 exports[`test/system/ts.js TAP TS System Tests > must match snapshot 1`] = `
-### Resolving resource in basic resourceProvider for file:///{fs}/presm/test/fixtures/tsmodule4.ts
-
-### Getting resource in basic resourceProvider for file:///{fs}/presm/test/fixtures/tsmodule4.ts
-
 42
 [ 42, 42, 42 ]
 This line was added by a post processor!!
@@ -17,14 +13,6 @@ This line was added by a post processor!!
 `
 
 exports[`test/system/ts.js TAP TS System Tests > must match snapshot 2`] = `
-### Resolving resource in basic resourceProvider for file:///{fs}/presm/test/fixtures/tsmodule2.ts
-
-### Getting resource in basic resourceProvider for file:///{fs}/presm/test/fixtures/tsmodule2.ts
-
-### Resolving resource in basic resourceProvider for /{fs}/presm/test/fixtures/tsmodule1.ts
-
-### Getting resource in basic resourceProvider for file:///{fs}/presm/test/fixtures/tsmodule1.ts
-
 This line was added by a post processor!!
 42
 This line was added by a post processor!!
