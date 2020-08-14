173f68f7ec79827fcaf0c854541325ea962c8e42
Julian Londono <jlondono@google.com>
2020-08-14 23:57:36 +0000

    Add bare specifier explanation

--- src/utils.js
+++ src/utils.js
@@ -10,6 +10,10 @@ export function isWrappedModule(extensions) {
   }
 }
 
+// Definition of bare specifiers according to:
+//  https://nodejs.org/api/esm.html#esm_terminology
+// NOTE: future implementation will follow:
+// https://html.spec.whatwg.org/multipage/webappapis.html#resolve-a-module-specifier
 export function isBareSpecifier(specifier) {
   if (specifier.startsWith('.') || specifier.startsWith('/')) {
     return false;
